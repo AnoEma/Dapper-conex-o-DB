8aa4f8a9a859cb9cad1129f7d781aaca61208730
ano.emangi <ano.emangi@minutoseguros.com.br>
2019-09-13 17:38:16 -0300

    Dapper .Net cadastro em DB

---
+++ Dapper2/Insert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper2
+{
+    class Insert
+    {
+        public int Id {get; set;}
+        public string Nome {get; set;}
+        public string Cpf {get; set;}
+        public string Numero {get; set;}
+        public char Sexo {get; set;}
+        public bool Adulto {get; set;}
+    }
+}
---
+++ Dapper2/Program.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper2
+{
+    class Program
+    {
+        static string strConexao = ConfigurationManager.ConnectionStrings["AcaoWeb"].ConnectionString;
+        static void Main(string[] args)
+        {
+            Acao();
+            Console.ReadKey();
+        }
+
+        private static void Acao()
+        {
+            using (var conn = new SqlConnection(strConexao))
+            {
+
+                var add = new Insert();
+                {
+
+                    Console.Write("Digite seu Nome: ");
+                    add.Nome = Console.ReadLine();
+                    Console.Write("Seu CPF: ");
+                    add.Cpf = Console.ReadLine();
+                    Console.Write("Seu Numero do Celular: ");
+                    add.Numero = Console.ReadLine();
+                    Console.Write("Sexo (M por Homem e F por Mulher: ");
+                    add.Sexo = char.Parse(Console.ReadLine());
+                    Console.Write("Voce é Adulto S por sim e N por não: ");
+                    string adt = Console.ReadLine();
+                    
+                    if (adt == "s" || adt == "S")
+                    {
+                        add.Adulto = true;
+                    }
+                    else if(adt == "n" || adt == "N")
+                    {
+                        add.Adulto = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("404");
+                    }
+                   
+                };
+
+                conn.Execute(@"insert into AcaoWeb(Nome,Cpf,Numero,Sexo,Adulto)values(@Nome, @Cpf, @Numero,@Sexo,@Adulto)", add);
+                Console.WriteLine("Cadastro realizado com sucesso\n*******************************");
+            }
+        }
+
+    }
+
+}
